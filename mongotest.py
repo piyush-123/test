be880011db01fa10bfffaf0ac756cbec0862db1a
Piyush Aggarwal <piyushricky1988@gmail.com>
2022-07-17 19:24:59 +0530

    This is a test push from system

---
+++ mongotest.py
@@ -0,0 +1,15 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://piyush1304:System909@cluster0.gocvn.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name" : "piyush",
+    "email" : "piyush@gmail.com",
+    "surname" : "aggarwal"
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
